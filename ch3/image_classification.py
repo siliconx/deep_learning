214c5e07a31e255d95bb1f54bddff233b60bddf3
siliconx <siliconx@163.com>
2019-10-17 15:10:56 +0800

    optimize

--- ch3/image_classification.py
+++ ch3/image_classification.py
@@ -1,20 +1,20 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import shutil
-from torchvision import transforms
-from torchvision import models
-import torch
-import torch.nn as nn
-from torch.optim import lr_scheduler
-from torch import optim
-from torchvision.datasets import ImageFolder
-from torchvision.utils import make_grid
-from torch.utils.data import DataLoader
+import pdb
 import time
+import torch
+import numpy as np
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from torch import optim
+from torchvision import models
+from torchvision import transforms
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
 
 is_cuda = False
+device = None
 if torch.cuda.is_available():
-    is_cuda = True
+    is_cuda = False
     count = torch.cuda.device_count()
     device = torch.device(count - 1)  # last gpu
 
@@ -30,11 +30,10 @@ def imshow(inp):
     plt.show()
 
 # Load data into tensor
-simple_transform = transforms.Compose([transforms.Resize((224,224)),
-    transforms.ToTensor(),
+simple_transform = transforms.Compose([transforms.Resize((224,224)), transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-train = ImageFolder('dogsandcats/train/', simple_transform)
-valid = ImageFolder('dogsandcats/valid/', simple_transform)
+train = ImageFolder('./dogsandcats/train/', simple_transform)
+valid = ImageFolder('./dogsandcats/valid/', simple_transform)
 print('train.class_to_idx:', train.class_to_idx)
 print('train.classes:', train.classes)
 
@@ -91,6 +90,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
             # Iterate over data
             for i, data in enumerate(dataloaders[phase]):
+                #pdb.set_trace()
                 print('\rNo.{}'.format(i), end='')
                 # get inputs
                 inputs, labels = data
@@ -118,15 +118,16 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects / dataset_sizes[phase]
-            print('\n%s -- Loss: %f, Acc: %f\n' % (phase, epoch_loss, epoch_acc))
+            print('\n{} -- Loss: {:.4f}, Acc: {.4f}\n'.format(phase, epoch_loss, epoch_acc))
 
             # deep copy the model
             if phase == 'valid' and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = model.state_dict()
     time_elapsed = time.time() - start
-    print('Training complete in %fm %fs' % (time_elapsed // 60, time_elapsed % 60))
-    print('Best accuracy: %f' % best_acc)
+    print('Training complete in {:.0f}m {:.4f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best accuracy: {:.4f}'.format(best_acc))
 
     # load best model weights
     model.load_state_dict(best_model_wts)
